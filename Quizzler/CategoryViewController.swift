79416bf0ca081079b0dd8bff075a107bf102815d
Moazam <mabass@nyit.edu>
2019-04-30 10:06:08 -0400

    Got rid of the errors. The login is not yet implemented.

---
+++ Quizzler/CategoryViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CategoryViewController.swift
+//  Custom Quiz
+//
+//  Created by Mir Moazam Abass on 4/23/19.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class CategoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+
+}
